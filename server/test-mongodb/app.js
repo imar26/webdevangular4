1c738c9193b9e771b6381473994fb76b9df1c7c4
Aadesh Randeria <ar.adaptivealgo@gmail.com>
2017-10-15 19:23:50 -0400

    mongodburi updated

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -13,9 +13,8 @@ module.exports = function(app)
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // use yours
+    connectionString += '@ds015962.mlab.com:15962/heroku_k87nrvcf'; // use yours
   }
-
   var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
